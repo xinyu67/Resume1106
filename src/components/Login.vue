35311e78063f28442d05044def372aabcc20c215
xinyu67 <silvia50727@gmail.com>
2023-11-06 23:35:10 +0800

    feat: :sparkles: 登入畫面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,84 +1,82 @@
 <template>
     <div class="login-page">
-      <h1>登录</h1>
-      <form @submit.prevent="login">
-        <div class="form-group">
-          <label for="username">用户名:</label>
-          <input type="text" id="username" v-model="username" required>
-        </div>
-        <div class="form-group">
-          <label for="password">密码:</label>
-          <input type="password" id="password" v-model="password" required>
-        </div>
-        <button type="submit">登录</button>
-      </form>
+        <h1>登入</h1>
+        <form @submit.prevent="login">
+            <div class="form-group">
+                <label for="username">帳號</label>
+                <input type="text" id="username" v-model="username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">密碼</label>
+                <input type="password" id="password" v-model="password" required>
+            </div>
+            <button type="submit">登入</button>
+        </form>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     data() {
-      return {
-        username: '',
-        password: ''
-      };
+        return {
+            username: '',
+            password: ''
+        };
     },
     methods: {
-      login() {
-        if (this.username === 'your_username' && this.password === 'your_password') {
-          // 实际应用中，这里需要进行用户身份验证
-          alert('登录成功');
-        } else {
-          alert('登录失败，请检查用户名和密码');
+        login() {
+            if (this.username === 'your_username' && this.password === 'your_password') {
+                alert('登入成功');
+            } else {
+                alert('登入失敗');
+            }
         }
-      }
     }
-  };
-  </script>
-  
-  <style>
-  .login-page {
+};
+</script>
+
+<style>
+.login-page {
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 50px;
-  }
-  
-  form {
+}
+
+form {
     width: 300px;
     background: #fff;
     border-radius: 5px;
     padding: 20px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
-  .form-group {
+}
+
+.form-group {
     margin: 10px 0;
-  }
-  
-  label {
+}
+
+label {
     display: block;
     font-weight: bold;
-  }
-  
-  input {
+}
+
+input {
     width: 100%;
     padding: 10px;
     border: 1px solid #ccc;
     border-radius: 5px;
-  }
-  
-  button {
+}
+
+button {
     background: #007BFF;
     color: #fff;
     padding: 10px 20px;
     border: none;
     border-radius: 5px;
     cursor: pointer;
-  }
-  
-  button:hover {
+}
+
+button:hover {
     background: #0056b3;
-  }
-  </style>
-  
+}
+</style>
